37c5fbc2f90086f3c4028771c00e0995fb381a02
main2410 <levchenvit@gmail.com>
2018-04-19 21:50:16 +0300

    refactor MainController, dis-servlet.xml

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import entity.User;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
@@ -19,7 +20,7 @@ public class MainController {
     public static final String USER = "user";
     
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @RequestMapping(name = MAIN_URL, method = RequestMethod.GET)
     public ModelAndView main(HttpServletRequest request,
